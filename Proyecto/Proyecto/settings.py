862bfd0dd450cafe51ad2b614ae06bd8f54161e3
Gonzalo <gonzalodiez97@gmail.com>
2023-06-13 16:02:57 -0300

    Arreglos en archivo settings y home.html

--- Proyecto/Proyecto/settings.py
+++ Proyecto/Proyecto/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Proyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["C:\\Users\\gonza\\Desktop\\Proyecto-coder\\Proyecto\\AppCoder\\plantillas\\AppCoder"],
+        'DIRS': ["C:/Users/gonza/Desktop/Proyecto-coder/Proyecto/AppCoder/plantillas/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
